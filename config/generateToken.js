fe0069ac2393e6d8b2ba434540236f6eaff79177
saiprasad <saiprasad.spsp90@gmail.com>
2025-04-29 10:09:19 +0530

    2025-04-29 generate token

--- config/generateToken.js
+++ config/generateToken.js
@@ -4,7 +4,7 @@ const generateToken = (res,userId)=>{
 const token = jwt.sign({userId},process.env.JWT_SECRET,{expiresIn:'1d'})
 
 res.cookie('jwt',token,{
-    httOnly:true,
+    httpOnly:true,
     secure:process.env.NODE_ENV !=='development',
     sameSite: 'strict',
     maxAge:24*60*60*1000
