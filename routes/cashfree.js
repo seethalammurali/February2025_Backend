67dc345c5d38760f799f7f1bdbeed12872324462
saiprasad <saiprasad.spsp90@gmail.com>
2025-08-29 10:42:59 +0530

    2025-08-29 pan

--- routes/cashfree.js
+++ routes/cashfree.js
@@ -15,16 +15,42 @@ const aadharVerification = asyncHandler(async(req,res)=>{
             'Content-Type':'application/json'
         }})
         console.log(response);
-        
+
         res.json({message:'Successfull',data:response.data})
-        
+
     } catch (err) {
         console.error(err);
         res.status(500).json({message:"Aadhar Verification failed",error:err})
-        
+
     }
   })
 
+
+const panVerification = asyncHandler(async(req,res)=>{
+    console.log(req.body);
+    const {pan} = req.body
+
+    try {
+        const response = await axios.post(`${process.env.PAPI}`,{pan:pan},{headers:{
+            'x-client-id':process.env.SECURE_ID_APP_ID,
+            'x-client-secret':process.env.SECURE_ID_SECRET_KEY,
+            'Content-Type':'application/json'
+        }})
+        console.log(response);
+
+        res.json({message:'Successfull',data:response.data})
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({message:"Pan Verification failed",error:err})
+
+    }
+  })
+
+
+
+
 router.post('/verify-aadhar',aadharVerification)
+router.post('/verify-pan',panVerification)
 
 module.exports = router;
